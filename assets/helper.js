adf522fb5fa65afb0fea709255f68eb291b537cf
marcellasyw <marcella.septania0@gmail.com>
2020-11-26 23:12:26 +0700

    cek note

--- assets/helper.js
+++ assets/helper.js
@@ -1,20 +1,58 @@
 let port = 'http://localhost:3000/'
 
+
+function restaurantPage() {
+    $('#main-page').hide()
+    showRestaurant()
+}
+
+function showRestaurant() {
+    $.ajax({
+        url : port + "restaurants/",
+        method : "get",
+        headers : {
+            access_token : localStorage.getItem("access_token")
+        }
+    })
+    .done(data => {
+        data.forEach(element => {
+            $('#').append(`
+            
+            `)
+        })
+    })
+    .fail(err => {
+        console.log(err)
+    })
+}
+
+function mainPage() {
+    $("#login-page").hide()
+    $("#regist-page").hide()
+    $('#main-page').show()
+    $("#btn-logout").show()
+    $('#show-data-recipe').hide()
+    $('#my-recipe').hide()
+}
+
 function loginPage(){
     $("#regist-page").hide()
     $("#btn-logout").hide()
+    $('#main-page').hide()
 }
 
 function register(){
     $("#login-page").hide()
     $("#regist-page").show()
+    $("#btn-logout").hide()
+    $('#main-page').hide()
 }
 
 function login() {
     const email = $("#loginEmail").val()
     const password = $("#loginPassword").val()
     $.ajax({
-        url : port + 'login/',
+        url : port + 'login',
         method : 'post',
         data : {
             email,
@@ -22,8 +60,7 @@ function login() {
         }
     })
     .done(data => {
-        let access_token = localStorage.setItem("access_token", data)
-        console.log(access_token);
+        localStorage.setItem("access_token", data)
     })
     .fail(err => {
         console.log(err)
@@ -35,7 +72,7 @@ function signup(){
     const email = $("#registEmail").val()
     const password = $("#registPassword").val()
     $.ajax({
-        url : port + 'signup/',
+        url : port + 'register',
         method : 'post',
         data : {
             username,
@@ -61,7 +98,7 @@ function logout(){
 
 function fetchRecipe(){
     $.ajax({
-        url : "",
+        url : port + "recipes",
         method : "get",
         headers : {
             access_token : localStorage.getItem('access_token')
@@ -69,8 +106,134 @@ function fetchRecipe(){
     })
     .done(data => {
         data.forEach(recipe => {
-            $('#mainhome').append(``)
+            $('#mainhome').append(`
+            <div class = "col mb-4">
+                <div class="card" style="width: 15rem;">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">${recipe.name}</h5>
+                  </div>
+                  <table class="table table-borderless">
+                      <th>Cooking Time</th>
+                      <td>${recipe.cooking_time}</td>
+                    </tr>
+                    <tr>
+                      <th>Type</th>
+                      <td>${recipe.type}</td>
+                    </tr>
+                  </table>
+                  <div class="card-body">
+                    <a class="btn btn-primary" id="btn-show" onclick = "recipeById(${recipe.id})">Show</a>
+                    <a class="btn btn-danger" id="btn-delete" onclick = "deleteRecipe(${recipe.id})">Delete</a>
+                  </div>
+                </div>
+              </div>
+            `)
         });
+    })
+    .fail(err => {
+        console.log(err)
+    })
+}
+
+function deleteRecipe(id){
+    $.ajax({
+        url : port + "recipes/" + id,
+        method : "delete",
+        headers : {
+            access_token : localStorage.getItem('access_token')
+        } 
+    })
+    ,done(data => {
+        myrecipe()
+    })
+    .fail(err => {
+        console.log(err);
+    })
+}
+
+function myrecipe() {
+    $('#main-page').hide()
+    $("#my-recipe").show()
+    // fetchRecipe()
+}
+
+function createRecipe() {
+    const name = $('#inputName').val()
+    const ingredients = $('#inputIngredients').val()
+    const steps = $('#inputSteps').val()
+    const portion = $('#inputPortion').val()
+    const cooking_time = $('#inputCookingTime').val()
+    const type = $('inputType').val()
+    $.ajax({
+        url : port + 'recipes',
+        method : 'post',
+        data : {
+            name,
+            ingredients,
+            steps,
+            portion,
+            cooking_time,
+            type
+        }
+    })
+    .done(data => {
+        myrecipe()
+    })
+    .fail(err => {
+        console.log(err);
+    })
+}
+
+function showRecipe(){
+    $('#main-page').hide()
+    $('#show-data-recipe').show()
+    recipeById()
+}
+
+
+function recipeById(id) {
+    $.ajax({
+        url : port + "recipes/" + id,
+        method : "get",
+        headers : {
+            access_token : localStorage.getItem('access_token')
+        }
+    })
+    .done(data => {
+       $('#show-data-recipe').append(`
+       <div class="card">
+       <div class="card-title text-center"> 
+         <h1> ${data.name} </h1> 
+       </div>
+       <div class="card-body">
+         <table class="table table-borderless">
+           <tr>
+             <th>Ingredients</th>
+             <td>${data.ingredients}</td>
+           </tr>
+           <tr>
+             <th>Steps</th>
+             <td>${data.steps}</td>
+           </tr>
+           <tr>
+             <th>Portion</th>
+             <td>${data.portion}</td>
+           </tr>
+           <tr>
+             <th>Cooking Time</th>
+             <td>${data.cooking_time}</td>
+           </tr>
+           <tr>
+             <th>Type</th>
+             <td>${data.type}</td>
+           </tr>
+         </table>
+       </div>
+       <div class="card-footer">
+         <a class="btn btn-primary" id="btn-back" onclick ="mainPage()">Back To Home</a>
+       </div>  
+     </div>   
+    `) 
     })
     .fail(err => {
         console.log(err)
---
+++ assets/note.md
@@ -0,0 +1,3 @@
+# masih rada salah
+1. data append buat restaurantpage(), showRestaurant(), myrecipe()
+2. 
